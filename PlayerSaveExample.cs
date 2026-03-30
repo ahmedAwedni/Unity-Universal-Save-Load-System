a87674565c52b619393e625429bd2cfe3816b6a6
Ahmed Awedni <151574595+ahmedAwedni@users.noreply.github.com>
2026-03-30 08:58:17 +0100

    Implement ISaveable interface for player data

---
+++ PlayerSaveExample.cs
@@ -0,0 +1,48 @@
+// 3. PlayerSaveExample.cs
+using UnityEngine;
+
+// Example of how to implement ISaveable on an existing script
+public class PlayerSaveExample : MonoBehaviour, ISaveable
+{
+    [SerializeField] private string uniqueSaveID = "Player_Stats";
+    public string SaveID => uniqueSaveID;
+
+    [Header("Player Data")]
+    public int level = 1;
+    public float health = 100f;
+    public Vector3 playerPosition;
+
+    // A private struct that holds ONLY the variables we want to save
+    [System.Serializable]
+    private struct PlayerData
+    {
+        public int level;
+        public float health;
+        public Vector3 position;
+    }
+
+    public string SaveState()
+    {
+        // Package our current variables into the struct
+        PlayerData data = new PlayerData
+        {
+            level = this.level,
+            health = this.health,
+            position = transform.position
+        };
+
+        // Return it as a JSON string
+        return JsonUtility.ToJson(data);
+    }
+
+    public void LoadState(string stateJson)
+    {
+        // Unpackage the JSON string back into our struct
+        PlayerData data = JsonUtility.FromJson<PlayerData>(stateJson);
+
+        // Apply it back to our actual variables
+        this.level = data.level;
+        this.health = data.health;
+        transform.position = data.position;
+    }
+}
